1f7245564c78d24dcb29837e2e164454bb8d391f
sayed <tkdsayed@gmail.com>
2022-12-19 13:08:20 +0100

    v4

---
+++ index.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv")
+const path = require("path")
+const express = require("express");
+
+dotenv.config()
+
+const port = process.env.port || 8000 
+
+
+const app = express();
+
+app.set('view engine', 'pug')
+
+
+//Ajouter un route handler pour la page d'accueil
+app.get("/",(req,res)=>{
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+//Ajouter un route handler pour la page signup
+app.get("/signup",(req,res)=>{
+    res.end("page sign up")
+})
+//Ajouter un route handler pour la page signin
+app.get("/signin",(req,res)=>{
+    res.end("page signin")
+})
+
+app.get("*",(req,res)=>{
+    res.end("404")
+})
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
+
