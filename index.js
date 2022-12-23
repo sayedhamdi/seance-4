9ec0508f54a8366330d6c9677aa875a78b513497
sayed <tkdsayed@gmail.com>
2022-12-23 12:41:30 +0100

    help me

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csv = require("csvtojson")
+const session = require('express-session')
+const cookieParser = require("cookie-parser");
+
+
 const csvWriter = createCsvWriter({
     path: 'users.csv',
     header: [
@@ -23,9 +27,19 @@ app.set('view engine', 'pug')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(session({
+    secret: '123456789',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true, maxAge: oneDay }
+}))
+app.use(cookieParser());
 
 //Ajouter un route handler pour la page d'accueil
 app.get("/",(req,res)=>{
+    console.log(req.session)
     res.render('index')
 })
 //Ajouter un route handler pour la page signup
@@ -36,9 +50,28 @@ app.get("/signup",(req,res)=>{
 app.get("/signin",(req,res)=>{
     res.render('signin')
 })
+app.get("/profile",(req,res)=>{
+    console.log(req.session.cookie)
+    res.render("profile")
+})
 
-app.post("/signin",(req,res)=>{
-    res.end("login successful")
+app.post("/signin",async (req,res)=>{
+    
+    let {email,password} = req.body;
+    if (!email || !password){
+        res.render("signin",{msg:"Please fill in your information"})
+        return
+    }
+    let users = await csv().fromFile("./users.csv")
+    let user = users.filter((user)=>user.email ==email && user.password==password)
+
+    if (user.length==0){
+        res.render("signin",{msg:"Email or password incorrect"})
+        return
+    }
+    res.cookie('userEmail', user[0].email)
+    res.redirect("/profile")
+    
 })
 app.post("/signup",async (req,res)=>{
     
@@ -54,7 +87,7 @@ app.post("/signup",async (req,res)=>{
     //creation compte
     let users = await csv().fromFile("./users.csv")
     console.log(users)
-    let userWithEmail = users.filter((user)=>user.EMAIL ==email) 
+    let userWithEmail = users.filter((user)=>user.email ==email) 
     if(userWithEmail.length>0){
         res.render("signup",{msg:"Email already exists use another email"})
         return
@@ -65,6 +98,7 @@ app.post("/signup",async (req,res)=>{
    
     res.render("signin",{msg:"your account has been created"})
 })
+
 app.get("*",(req,res)=>{
     res.end("404")
 })
